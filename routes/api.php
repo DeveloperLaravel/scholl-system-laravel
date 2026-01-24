74adc138463ac65bf3af6a494f5510f9f2b7e1a7
Abbes <abbes.dev@gmail.com>
2026-01-24 13:48:50 +0200

    Route

--- routes/api.php
+++ routes/api.php
@@ -16,15 +16,13 @@ use App\Http\Controllers\Api\AuthController;
 | - بيانات المستخدم (user)
 |
 */
+Route::get('/dashboard', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/dashboard', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', fn (Request $r) => $r->user());
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
